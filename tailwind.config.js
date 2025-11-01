2409c9c5f160c03c0a2d72373b2649c7f94e0229
muriz-dev <mhmdrizki.dev@gmail.com>
2025-11-01 18:59:33 +0700

    Customize Tailwind config based on DMS brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ import forms from '@tailwindcss/forms';
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
+        './storage/framework/views/*.blade.php',
         './resources/views/**/*.blade.php',
     ],
 
@@ -13,6 +13,25 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                // DMS Brand Colors
+                'dms-orange': '#FF6900',
+                'dms-black': '#000000',
+                'dms-white': '#FFFFFF',
+                // Extended Orange Palette
+                'orange': {
+                    50: '#fff7ed',
+                    100: '#ffedd5',
+                    200: '#fed7aa',
+                    300: '#fdba74',
+                    400: '#fb923c',
+                    500: '#FF6900', // Primary DMS Orange
+                    600: '#ea580c',
+                    700: '#c2410c',
+                    800: '#9a3412',
+                    900: '#7c2d12',
+                },
             },
         },
     },
